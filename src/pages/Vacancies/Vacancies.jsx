1410d288174fc5befd3f70c3f8cd7656f54b51d4
Vika-Derkach <queenviktoriyphp@gmail.com>
2022-05-11 14:57:02 +0300

    server

--- src/pages/Vacancies/Vacancies.jsx
+++ src/pages/Vacancies/Vacancies.jsx
@@ -1,7 +1,19 @@
+import { Button, Card, Container } from "@mui/material";
 import React from "react";
 
 const Vacancies = () => {
-  return <div>Vacancies</div>;
+  return (
+    <div>
+      <Container maxWidth="md">
+        {" "}
+        <h1>Вакансии и отклики</h1>
+        <Button color="success" variant="contained">
+          Создать вакансию
+        </Button>
+        <Card variant="outlined">fg</Card>
+      </Container>
+    </div>
+  );
 };
 
 export { Vacancies };
